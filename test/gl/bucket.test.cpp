aaa72403e502adf1d2506b03363e15e18afb8930
Andrew Hay Kurtz <andrew.hay.kurtz@gmail.com>
2019-10-30 14:53:45 -0700

    Add sort key to CircleBucket segments

--- test/gl/bucket.test.cpp
+++ test/gl/bucket.test.cpp
@@ -51,6 +51,7 @@ TEST(Buckets, CircleBucket) {
     ASSERT_FALSE(bucket.hasData());
     ASSERT_FALSE(bucket.needsUpload());
 
+    // CircleBucket::addFeature() is a no-op.
     GeometryCollection point { { { 0, 0 } } };
     bucket.addFeature(StubGeometryTileFeature{{}, FeatureType::Point, point, properties},
                       point,
@@ -58,6 +59,10 @@ TEST(Buckets, CircleBucket) {
                       PatternLayerMap(),
                       0,
                       CanonicalTileID(0, 0, 0));
+    ASSERT_FALSE(bucket.hasData());
+    ASSERT_FALSE(bucket.needsUpload());
+
+    bucket.segments.emplace_back(0, 0);
     ASSERT_TRUE(bucket.hasData());
     ASSERT_TRUE(bucket.needsUpload());
 
