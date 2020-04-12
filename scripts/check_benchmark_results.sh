a015e0df957df24ccd20ea8e6ca3c82d37b1e32f
Juha Alanen <juha.alanen@mapbox.com>
2020-04-12 19:38:14 +0300

    Script update

--- scripts/check_benchmark_results.sh
+++ scripts/check_benchmark_results.sh
@@ -4,8 +4,13 @@
 # check_benchmark_results.sh <baseline> <results> <comparison report in txt> <comparison report in html>
 
 # Benchmark comparison script uses a hard-coded 5% threshold by default, but it causes random failures
-# Increase the threshold to 7%
-sed -i s/"if res > 0.05"/"if res > 0.07"/ $PWD/vendor/benchmark/tools/gbench/report.py
+# Increase the threshold to 10%
+
+if [ "$(uname)" == "Darwin" ]; then
+  sed -i '' s/"if res > 0.05"/"if res > 0.1"/ $PWD/vendor/benchmark/tools/gbench/report.py
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  sed -i s/"if res > 0.05"/"if res > 0.1"/ $PWD/vendor/benchmark/tools/gbench/report.py
+fi
 
 # Run the bencmark comparison
 vendor/benchmark/tools/compare.py benchmarks $1 $2 > $3
@@ -16,7 +21,7 @@ tests=`fgrep mean $3 | cut -c1-85 | grep 91m`
 if [ -z "$tests" ]
 then
       echo "" >> $3
-      echo "PASSED, all benchmarks within 7% threshold" >> $3
+      echo "PASSED, all benchmarks within 10% threshold" >> $3
 
       ansi2html < $3 > $4
       exit 0
@@ -24,7 +29,7 @@ else
       test_names=`fgrep mean $3 | cut -c1-85 | grep 91m | cut -f1 -d' '`
 
       echo "" >> $3
-      echo "FAILED, following benchmarks are not within 7% threshold:" >> $3
+      echo "FAILED, following benchmarks are not within 10% threshold:" >> $3
       echo "" >> $3
       for t in $test_names;
       do
